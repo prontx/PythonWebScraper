e9273f41bdae23b3147e65cfc72203fba01b892f
prontx <79801523+prontx@users.noreply.github.com>
2021-08-24 17:31:34 +0200

    Same drill for jobfinder

--- jobfinder.py
+++ jobfinder.py
@@ -1,24 +1,84 @@
 # Webscraper Application Implemented Using Python
-# jobfinder.py - contains function definition for script mode 1
+# jobfinder.py - contains function definition for script mode 1.
+#                Asks for job name, job location, prints open positions
 # Author: Matsvei Hauryliuk, FIT VUT Student
 # Github: @prontx
 
 import requests
 from bs4 import BeautifulSoup
+import re
+
+# To keep track of invalid inputs, once the number reaches 10 the application exits
+invalidEntryCounter = 0
 
 def jobFinder():
     # Basic link that will be added to later on
     link = 'https://www.linkedin.com/jobs/search?'
     print('Enter the job you are looking for: ')
-    jobName = str(input())
+
+    inputToBeChecked = input()
+
+    # For global variables to be changed in all scopes
+    global invalidEntryCounter
+
+    # Handling cases when more than thirty digits are input
+    if len(inputToBeChecked) > 30:
+        print('Cannot have that input, try again\n')
+        invalidEntryCounter += 1
+        if invalidEntryCounter == 10:
+            print('Too many invalid entries, exiting the app')
+            exit(1)
+        return
+
+    # Making sure the input is a string, possibly with whitespaces
+    if re.match('[A-Z]*', inputToBeChecked):
+        jobName = str(inputToBeChecked)
+    else:
+        print('Cannot have that input, try again\n')
+        invalidEntryCounter += 1
+        if invalidEntryCounter == 10:
+            print('Too many invalid entries, exiting the app')
+            exit(1)
+        return
+
     print('Enter the location you would want to work from: ')
-    jobPreferredLocation = str(input())
+
+    inputToBeChecked = input()
+
+    # Handling cases when more than thirty digits are input
+    if len(inputToBeChecked) > 30:
+        print('Cannot have that input, try again\n')
+        invalidEntryCounter += 1
+        if invalidEntryCounter == 10:
+            print('Too many invalid entries, exiting the app')
+            exit(1)
+        return
+
+    # Making sure the input is a string, possibly with whitespaces
+    if re.match('[A-Z]*', inputToBeChecked):
+        jobPreferredLocation = str(inputToBeChecked)
+    else:
+        print('Cannot have that input, try again\n')
+        invalidEntryCounter += 1
+        if invalidEntryCounter == 10:
+            print('Too many invalid entries, exiting the app')
+            exit(1)
+        return
+
     # Personalizing the URL
     link += f'keywords = {jobName}&location={jobPreferredLocation}'
     searchResult = requests.get(link)
-    parsedPage = BeautifulSoup(searchResult.content, 'html.parser')
+    if searchResult:
+        parsedPage = BeautifulSoup(searchResult.content, 'html.parser')
+    else:
+        print('Something went wrong\n')
+        exit(1)
     # Returns a list of active positions
-    jobPositions = parsedPage.find_all("div", class_="base-card base-card--link base-search-card base-search-card--link job-search-card")
+    if parsedPage:
+        jobPositions = parsedPage.find_all("div", class_="base-card base-card--link base-search-card base-search-card--link job-search-card")
+    else:
+        print('Something went wrong\n')
+        exit(1)
     # A counter to keep track of job number
     jobCounter = 0
     print()
