8534f810aac8ed2eb70661e3d80d10b06b342fd9
prontx <79801523+prontx@users.noreply.github.com>
2021-08-24 17:16:55 +0200

    Improved covid module with error handling and regexes

--- covid.py
+++ covid.py
@@ -1,20 +1,60 @@
 # Webscraper Application Implemented Using Python
-# covid.py - contains function definition for script mode 2
+# covid.py - contains function definition for script mode 2.
+#            Prints latest covid info about a country.
 # Author: Matsvei Hauryliuk, FIT VUT Student
 # Github: @prontx
 
 import requests
 from bs4 import BeautifulSoup
+import re
+
+# To keep track of invalid inputs, once the number reaches 10 the application exits
+invalidEntryCounter = 0
 
 def covidStats():
     link = "https://www.worldometers.info/coronavirus/"
     print('Enter the country: ')
-    country = str(input())
+    inputToBeChecked = input()
+
+    # For global variables to be changed in all scopes
+    global invalidEntryCounter
+
+    # Handling cases when more than twenty digits are input
+    if len(inputToBeChecked) > 20:
+        print('Cannot have that input, try again\n')
+        invalidEntryCounter += 1
+        if invalidEntryCounter == 10:
+            print('Too many invalid entries, exiting the app')
+            exit(1)
+        return
+
+    # Making sure the input is a string, possibly with whitespaces
+    if re.match('[A-Z]*', inputToBeChecked):
+        country = str(inputToBeChecked)
+    else:
+        print('Cannot have that input, try again\n')
+        invalidEntryCounter += 1
+        if invalidEntryCounter == 10:
+            print('Too many invalid entries, exiting the app')
+            exit(1)
+        return
+
     unparsedPage = requests.get(link)
     parsedPage = BeautifulSoup(unparsedPage.content, 'html.parser')
     selectedCountry = parsedPage.find("a", text=country)
-    parentElement = selectedCountry.parent.parent
-    statistics = parentElement.find_all("td")
+
+    if selectedCountry:
+        parentElement = selectedCountry.parent.parent
+    else:
+        print('Something went wrong\n')
+        exit(1)
+
+    if parentElement:
+        statistics = parentElement.find_all("td")
+    else:
+        print('Something went wrong\n')
+        exit(1)
+
     print()
     counter = 0    
     for element in statistics:
@@ -44,3 +84,6 @@ def covidStats():
             print('\t' + 'Number of tests per 1M population:  ' + element.text)
         counter += 1
     print()
+
+if __name__ == "__main__":
+    covidStats()
