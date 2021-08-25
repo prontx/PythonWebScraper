8214ac636ada9c6ef5579cc81d7ff285dd48602b
prontx <79801523+prontx@users.noreply.github.com>
2021-08-25 11:22:12 +0200

    Updated reddit

--- reddit.py
+++ reddit.py
@@ -1,5 +1,7 @@
 # Webscraper Application Implemented Using Python
 # reddit.py - contains function definition for script modes 5 and 6
+#             Mode 5 prints best posts on a given subreddit,
+#             mode 6 prints the comments to a given post.
 # Author: Matsvei Hauryliuk, FIT VUT Student
 # Github: @prontx
 
@@ -8,6 +10,7 @@ from bs4 import BeautifulSoup
 import praw
 import os
 from dotenv import load_dotenv
+import re
 
 load_dotenv()
 CLIENT_ID         = os.getenv('CLIENT_ID')
@@ -16,22 +19,75 @@ REDDIT_USER_AGENT = os.getenv('REDDIT_USER_AGENT')
 
 reddit = praw.Reddit(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, user_agent=REDDIT_USER_AGENT)
 
+# To keep track of invalid inputs, once the number reaches 10 the application exits
+invalidEntryCounter = 0
+
 def mostPopularPosts():
     print()
     print('Enter a subreddit that interests you: ')
-    enteredSubreddit = str(input())
+
+    inputToBeChecked = input()
+
+    # For global variables to be changed in all scopes
+    global invalidEntryCounter
+
+    # Handling cases when more than thirty digits are input
+    if len(inputToBeChecked) > 30:
+        print('Cannot have that input, try again\n')
+        invalidEntryCounter += 1
+        if invalidEntryCounter == 10:
+            print('Too many invalid entries, exiting the app')
+            exit(1)
+        return
+
+    # Making sure the input is a string, possibly with whitespaces
+    if re.match('[A-Z]*', inputToBeChecked):
+        enteredSubreddit = str(inputToBeChecked)
+    else:
+        print('Cannot have that input, try again\n')
+        invalidEntryCounter += 1
+        if invalidEntryCounter == 10:
+            print('Too many invalid entries, exiting the app')
+            exit(1)
+        return
+
     print()
     print('Enter a number of top posts you wish to see: ')
-    enteredNumberOfPosts = int(input())
+
+    inputToBeChecked = input()
+
+    # Handling cases when more than three digits are input
+    if len(inputToBeChecked) > 3:
+        print('Cannot have that input, try again\n')
+        invalidEntryCounter += 1
+        if invalidEntryCounter == 10:
+            print('Too many invalid entries, exiting the app')
+            exit(1)
+        return
+
+    # Making sure the input is an integer, possibly with whitespaces
+    if re.match('[0-9]*', inputToBeChecked):
+        enteredNumberOfPosts = int(inputToBeChecked)
+    else:
+        print('Cannot have that input, try again\n')
+        invalidEntryCounter += 1
+        if invalidEntryCounter == 10:
+            print('Too many invalid entries, exiting the app')
+            exit(1)
+        return
 
     newPosts = reddit.subreddit(enteredSubreddit).top(limit=enteredNumberOfPosts)
-    postCounter = 0
-    print()
-    for newPost in newPosts:
-        postCounter += 1
-        print(str(postCounter) + " " + newPost.title)
-        print('\t' + str(newPost.score))
+    if newPosts:
+        postCounter = 0
         print()
+        for newPost in newPosts:
+            postCounter += 1
+            print(str(postCounter) + " " + newPost.title)
+            print('\t' + str(newPost.score))
+            print()
+    else:
+        print('Something went wrong\n')
+        exit(1)
 
 
 def postComments():
@@ -39,8 +95,16 @@ def postComments():
     print('Please enter the URL of the post you wish to scrape: ')
     link = str(input())
     submission = reddit.submission(url=link)
-    submission.comments.replace_more(limit=0)
+    if submission:
+        submission.comments.replace_more(limit=0)
+    else:
+        print('Something went wrong\n')
+        exit(1)
     print()
     for top_level_comment in submission.comments():
         print(top_level_comment.body)
     print()    
+
+if __name__ == "__main__":
+    mostPopularPosts()
+    postComments()
